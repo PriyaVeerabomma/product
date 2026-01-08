e07923221cdc52e0161d70bd3e511114f1786672
PriyaVeerabomma <veerabomma.s@northeastern.edu>
2026-01-08 15:03:10 -0800

    fix-metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,7 @@ const poppins = Poppins({
 
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://saipriya.vercel.app"),
   title: "Sai Priya Veerabomma | Software Engineer",
   description:
     " Sai Priya Veerabomma is a Software Engineer with experience building production-grade AI and backend systems. Specialized in Java, Python, RAG, ReRAG, prompt engineering,React, Next.js, FastAPI, LangGraph,Langchain, Gen-AI, AWS, GCP, Cloud native, data structures algorithms, leetcode and scalable cloud-native architectures, with experience delivering low-latency, reliable systems in academic and industry settings.",
